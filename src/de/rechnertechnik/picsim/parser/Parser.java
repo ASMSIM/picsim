13cbfd9e3fb7fcc5b336563cc69121dd5fe591eb
michael <michael@ubuntu>
2013-04-15 15:47:07 +0200

    OPEN - task 7: Parser schreiben http://github.com/ASMSIM/picsim/issues/issue/7
    Added Arraylist instead of Array

--- src/de/rechnertechnik/picsim/parser/Parser.java
+++ src/de/rechnertechnik/picsim/parser/Parser.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * 
@@ -13,58 +14,60 @@ import java.io.IOException;
  */
 public class Parser {
 
-	// TODO ARRAYLIST?
-	// AUS DATEI AUSLESEN, wie viel Zeilen vorhanden sind -> Array anpassen
-	private String[] progLine = new String[200];
+	ArrayList<String> progLine = new ArrayList<String>();
+	
 	private File file;
-	
-	
+
 	/**
 	 * Create Parser and init with File
 	 * 
-	 * @param String LST File
+	 * @param String
+	 *            LST File
 	 */
 	public Parser(String pathname) {
 		this.file = new File(pathname);
 		try {
 			readFile();
-		} catch (FileNotFoundException e) {
-			System.err.println("File: "+pathname+" not found!");
+		}
+		catch(FileNotFoundException e) {
+			System.err.println("File: " + pathname + " not found!");
 			e.printStackTrace();
 		}
-	}
-	
-	
-	
-	private void readLine() {
-		// TODO Auto-generated method stub
-	}
-	
-	
-	public void readFile() throws FileNotFoundException{
 		
+		
+		System.out.println("Debug DELME");
+	}
+
+	/**
+	 * Reads single line and puts its Content into the progLine Array
+	 * 
+	 * @param content
+	 */
+	private void readLine(String content) {
+		progLine.add(content);
+	}
+
+	public void readFile() throws FileNotFoundException {
+
 		FileReader fileReader = new FileReader(file);
 		BufferedReader buffReader = new BufferedReader(fileReader);
-		
+		String thisLine = "";
+
 		try {
-			
-			while(buffReader.readLine() != null){
-				System.out.println(buffReader.readLine());
+			while((thisLine = buffReader.readLine()) != null) {
+				// TODO Insert Logger
+				System.out.println(thisLine);
+
+				// Reads Current Line and calls readline with its Content
+				readLine(thisLine);
 			}
-			
-			
-		} catch (IOException e) {
+
+		}
+		catch(IOException e) {
 			System.err.println("Sorry I/O Error");
 			e.printStackTrace();
 		}
-		
-		
-	
+
 	}
-	
-	
-	
-	
-	
-	
+
 }
