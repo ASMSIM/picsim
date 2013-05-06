ffecb559a074420ae457862ca54694d520e32671
michael <michael@ubuntu>
2013-05-06 14:34:58 +0200

    GUI Interfaces erstellt

---
+++ src/de/rechnertechnik/picsim/gui/IGUI.java
@@ -0,0 +1,9 @@
+package de.rechnertechnik.picsim.gui;
+
+import java.util.ArrayList;
+
+public interface IGUI {
+
+	public void showSourcecode(ArrayList<String> sourceLine);
+	
+}
---
+++ src/de/rechnertechnik/picsim/gui/IProzessor.java
@@ -0,0 +1,7 @@
+package de.rechnertechnik.picsim.gui;
+
+public interface IProzessor {
+	
+	public void nextStep();
+
+}
