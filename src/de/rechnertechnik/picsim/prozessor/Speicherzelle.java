c97f885aefbf7c047f8cef2f5f6eef5d86bbd30d
michael <michael@ubuntu>
2013-05-13 12:25:51 +0200

    Speicherzelle erweitert um getBit()

--- src/de/rechnertechnik/picsim/prozessor/Speicherzelle.java
+++ src/de/rechnertechnik/picsim/prozessor/Speicherzelle.java
@@ -9,22 +9,37 @@ public class Speicherzelle {
 		this.speicherzelle = new Integer(value);
 	}
 
-
 	private void checkValid(Integer value) throws MemoryOutOfRangeException {
-		if (value > 65535 || value < 0) {
+		if(value > 65535 || value < 0) {
 			throw new MemoryOutOfRangeException();
 		}
 	}
-	
-	
+
 	public Integer getValue() {
 		return this.speicherzelle;
 	}
-	
+
 	public void setWert(Integer value) throws MemoryOutOfRangeException {
 		checkValid(value);
 		this.speicherzelle = value;
 	}
-	
+
+	public boolean getBit(Integer bitNr) {
+
+		if((this.getValue() & (1 << bitNr)) != 0) {
+			// The bit was set
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public void incRegister() {
+		this.speicherzelle++;
+		if(this.speicherzelle == 256) {
+			this.speicherzelle = 0; // Überlauf
+		}
+	}
 
 }
